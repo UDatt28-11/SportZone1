080bfb861d9a686d4a6529c3eb60eb23e78e3fc6
HoangHung231 <ui213454@gmail.com>
2025-04-09 11:19:41 +0700

    check image chi tiet

--- admin/views/sanpham/_partial_image_list.php
+++ admin/views/sanpham/_partial_image_list.php
@@ -1,3 +1,6 @@
 <?php foreach ($images as $img): ?>
     <img onclick="changeMainImage(this)" src="<?= BASE_URL . $img['link_hinh_anh'] ?>" alt="" style="width: 120px; height:120px; margin-right: 10px; border: 1px solid gray;">
 <?php endforeach ?>
+<?php if(empty($images)){
+    echo '<div class="alert alert-danger" role="alert">No images</div>';
+}
